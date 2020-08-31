e1f091ec615d52a2f1720ee14fa898d1daf3407f
Avinash6798 <68348087+Avinash6798@users.noreply.github.com>
2020-08-31 23:55:58 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,163 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import os
+import cv2
+import random
+from requests import get
+import wikipedia
+import webbrowser
+# import pywhatkit as kit
+import smtplib
+import sys
+import time
+
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices');
+# print(voices[0].id)
+engine.setProperty('voices', voices[len(voices) - 1].id)
+
+#text to speech
+def speak(audio):
+    engine.say(audio)
+    print(audio)
+    engine.runAndWait()
+
+
+#To convert voice into text
+def  takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,timeout=5,phrase_time_limit=8)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+
+    except Exception as e:
+        speak("Say that again please...")
+        return "none"
+    return query
+
+#to wish
+def wish():
+    hour = int(datetime.datetime.now().hour)
+    tt = time.strftime("%I:%M %p")
+
+    if hour >= 0 and hour <= 12:
+        speak(f"good morning, its {tt}")
+    elif hour >= 12 and hour <= 18:
+        speak(f"good afternoon, its {tt}")
+    else:
+        speak(f"good evening, its {tt}")
+    speak("i am jarvis sir. please tell me how may i help you")
+
+#to send email
+def sendEmail(to,content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('YOUR EMAIL ADDRESS', 'YOUR PASSWORD')
+    server.sendmail('YOUR EMAIL ADDRESS', to, content)
+    server.close()
+
+
+if __name__ == "__main__":
+    wish()
+    while True:
+    # if 1:
+
+        query = takecommand().lower()
+
+        #logic building for tasks
+
+        if "open notepad" in query:
+            npath = "C:\\Windows\\system32\\notepad.exe"
+            os.startfile(npath)
+
+        elif "open adobe reader" in query:
+            apath = "C:\\Program Files (x86)\\Adobe\\Reader 11.0\\Reader\\AcroRd32.exe"
+            os.startfile(apath)
+
+        elif "open command prompt" in query:
+            os.system("start cmd")
+
+        elif "open camera" in query:
+            cap = cv2.VideoCapture(0)
+            while True:
+                ret, img = cap.read()
+                cv2.imshow('webcam', img)
+                k = cv2.waitKey(50)
+                if k==27:
+                    break;
+            cap.release()
+            cv2.destroyAllWindows()
+
+        elif "play music" in query:
+            music_dir = "E:\\music"
+            songs = os.listdir(music_dir)
+            # rd = random.choice(songs)
+            for song in songs:
+                if song.endswith('.mp3'):
+                    os.startfile(os.path.join(music_dir, song))
+
+
+
+        elif "ip address" in query:
+            ip = get('https://api.ipify.org').text
+            speak(f"your IP address is {ip}")
+
+        elif "wikipedia" in query:
+            speak("searching wikipedia....")
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query, sentences=2)
+            speak("according to wikipedia")
+            speak(results)
+            # print(results)
+
+        elif "open youtube" in query:
+            webbrowser.open("www.youtube.com")
+
+        elif "open facebook" in query:
+            webbrowser.open("www.facebook.com")
+
+        elif "open stackoverflow" in query:
+            webbrowser.open("www.stackoverflow.com")
+
+        elif "open google" in query:
+            speak("sir, what should i search on google")
+            cm = takecommand().lower()
+            webbrowser.open(f"{cm}")
+
+        elif "send whatsapp message" in query:
+            kit.sendwhatmsg("+919992432180", "this is testing protocol",4,13)
+            time.sleep(120)
+            speak("message has been sent")
+
+        elif "song on youtube" in query:
+            kit.playonyt("see you again")
+
+        elif "email to avinash" in query:
+            try:
+                speak("what should i say?")
+                content = takecommand().lower()
+                to = "EMAIL OF THE OTHER PERSON"
+                sendEmail(to,content)
+                speak("Email has been sent to avinash")
+
+            except Exception as e:
+                print(e)
+                speak("sorry sir, i am not able to sent this mail to avi")
+
+        elif "no thanks" in query:
+            speak("thanks for using me sir, have a good day.")
+            sys.exit()
+
+        # speak("sir, do you have any other work")
+
