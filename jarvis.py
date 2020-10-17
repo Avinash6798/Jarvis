e158977ee2f99e467a7b1ccaaf66c7c82f1a06dc
Avinash <68348087+Avinash6798@users.noreply.github.com>
2020-10-17 14:05:28 +0530

    Update jarvis.py
    Adding some New functions to execute as like Window close, Alarm, Find a joke, Switch thw window,  News update, Email new update etc.

--- jarvis.py
+++ jarvis.py
@@ -16,6 +16,14 @@ import webbrowser
 import smtplib
 import sys
 import time
+import pyjokes
+import requests
+import pyautogui
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
 
 
 engine = pyttsx3.init('sapi5')
@@ -61,6 +69,7 @@ def wish():
         speak(f"good evening, its {tt}")
     speak("i am jarvis sir. please tell me how may i help you")
 
+"""    
 #to send email
 def sendEmail(to,content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -69,6 +78,26 @@ def sendEmail(to,content):
     server.login('YOUR EMAIL ADDRESS', 'YOUR PASSWORD')
     server.sendmail('YOUR EMAIL ADDRESS', to, content)
     server.close()
+ """
+ 
+ 
+
+#for news updates
+def news():
+    main_url = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey="YOUR_API_HERE"'
+
+    main_page = requests.get(main_url).json()
+    # print(main_page)
+    articles = main_page["articles"]
+    # print(articles)
+    head = []
+    day=["first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth"]
+    for ar in articles:
+        head.append(ar["title"])
+    for i in range (len(day)):
+        # print(f"today's {day[i]} news is: ", head[i])
+        speak(f"today's {day[i]} news is: {head[i]}")
+
 
 
 if __name__ == "__main__": #main program
@@ -142,7 +171,7 @@ if __name__ == "__main__": #main program
             webbrowser.open(f"{cm}")
 
         elif "send whatsapp message" in query:
-            kit.sendwhatmsg("+919992432180", "this is testing protocol",4,13)
+            kit.sendwhatmsg("+91_To_number_you_want_to_send", "this is testing protocol",4,13)
             time.sleep(120)
             speak("message has been sent")
 
@@ -161,7 +190,7 @@ if __name__ == "__main__": #main program
 
             time.sleep(timing)
             speak('Your time has been finished sir')
-         
+         """
         elif "email to avinash" in query:
             try:
                 speak("what should i say?")
@@ -173,10 +202,118 @@ if __name__ == "__main__": #main program
             except Exception as e:
                 print(e)
                 speak("sorry sir, i am not able to sent this mail to avi")
+                
+              """
 
         elif "no thanks" in query:
             speak("thanks for using me sir, have a good day.")
             sys.exit()
+            
+
+
+#to close any application
+        elif "close notepad" in query:
+            speak("okay sir, closing notepad")
+            os.system("taskkill /f /im notepad.exe")
+
+#to set an alarm
+        elif "set alarm" in query:
+            nn = int(datetime.datetime.now().hour)
+            if nn==22: 
+                music_dir = 'E:\\music'
+                songs = os.listdir(music_dir)
+                os.startfile(os.path.join(music_dir, songs[0]))
+#to find a joke
+        elif "tell me a joke" in query:
+            joke = pyjokes.get_joke()
+            speak(joke)
+
+        elif "shut down the system" in query:
+            os.system("shutdown /s /t 5")
+
+        elif "restart the system" in query:
+            os.system("shutdown /r /t 5")
+
+        elif "sleep the system" in query:
+            os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+
+
+
+###########################################################################################################################################
+###########################################################################################################################################
+
+
+
+        elif 'switch the window' in query:
+            pyautogui.keyDown("alt")
+            pyautogui.press("tab")
+            time.sleep(1)
+            pyautogui.keyUp("alt")
+                   
+
+        elif "tell me news" in query:
+            speak("please wait sir, feteching the latest news")
+            news()
+
+
+        elif "email to avinash" in query:
+               
+            speak("sir what should i say")
+            query = takecommand().lower()
+            if "send a file" in query:
+                email = 'your@gmail.com' # Your email
+                password = 'your_pass' # Your email account password
+                send_to_email = 'To_person@gmail.com' # Whom you are sending the message to
+                speak("okay sir, what is the subject for this email")
+                query = takecommand().lower()
+                subject = query   # The Subject in the email
+                speak("and sir, what is the message for this email")
+                query2 = takecommand().lower()
+                message = query2  # The message in the email
+                speak("sir please enter the correct path of the file into the shell")
+                file_location = input("please enter the path here")    # The File attachment in the email
+
+                speak("please wait,i am sending email now")
+
+                msg = MIMEMultipart()
+                msg['From'] = email
+                msg['To'] = send_to_email
+                msg['Subject'] = subject
+
+                msg.attach(MIMEText(message, 'plain'))
+
+                # Setup the attachment
+                filename = os.path.basename(file_location)
+                attachment = open(file_location, "rb")
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(attachment.read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+                # Attach the attachment to the MIMEMultipart object
+                msg.attach(part)
+
+                server = smtplib.SMTP('smtp.gmail.com', 587)
+                server.starttls()
+                server.login(email, password)
+                text = msg.as_string()
+                server.sendmail(email, send_to_email, text)
+                server.quit()
+                speak("email has been sent to avinash")
+
+            else:                
+                email = 'your@gmail.com' # Your email
+                password = 'your_pass' # Your email account password
+                send_to_email = 'To_person@gmail.com' # Whom you are sending the message to
+                message = query # The message in the email
+
+                server = smtplib.SMTP('smtp.gmail.com', 587) # Connect to the server
+                server.starttls() # Use TLS
+                server.login(email, password) # Login to the email server
+                server.sendmail(email, send_to_email , message) # Send the email
+                server.quit() # Logout of the email server
+                speak("email has been sent to avinash")
+            
 
         # speak("sir, do you have any other work")
 
